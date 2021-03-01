fef5bac5264fac600a328092ec4444a6a77b7f79
Usama-PC <usama@movement.college>
2021-03-01 23:43:36 +0500

    Add project files.

---
+++ PaymentProcessor.Models.DTO/PaymentRequestDto.cs
@@ -0,0 +1,26 @@
+﻿using PaymentProcessor.Models.DTO.Validation;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaymentProcessor.Models.DTO
+{
+    public class PaymentRequestDto
+    {
+        [Required]
+        [CreditCard]
+        public string CreditCardNumber { get; set; }
+        [Required]
+        public string CardHolder { get; set; }
+        [Required]
+        [DateNotInPastValidation(ErrorMessage = "ExpirationDate is Invalid")]
+        
+        public DateTime ExpirationDate { get; set; }
+
+        [StringLength(maximumLength: 3, MinimumLength =3)]
+        public string SecurityCode { get; set; }
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Amount { get; set; }
+
+    }
+}
---
+++ PaymentProcessor.WebApi/Controllers/PayController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PaymentProcessor.Models.DTO;
+using PaymentProcessor.Services;
+
+namespace PaymentProcessor.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PayController : ControllerBase
+    {
+        private readonly ILogger<PayController> _logger;
+        private readonly IPaymentRequestService _paymentRequestService;
+
+        public PayController(IPaymentRequestService paymentRequestService, ILogger<PayController> logger)
+        {
+            _logger = logger;
+            _paymentRequestService = paymentRequestService;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Payment Processor is online";
+        }
+
+        [Route("ProcessPayment")]
+        [HttpPost]
+        public async Task<IActionResult> ProcessPayment(PaymentRequestDto paymentRequest)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var paymentState = await _paymentRequestService.Pay(paymentRequest);
+                    if (!(paymentState.PaymentState == PaymentStateEnum.Processed))
+                        return StatusCode(400, "400 bad request");
+                    return StatusCode(200, "200 Ok");
+                }
+                else
+                    return StatusCode(400, "400 bad request");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                return StatusCode(500, "500 internal server error");
+            }
+        }
+    }
+}
